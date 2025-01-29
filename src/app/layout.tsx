7569ce72c23457f9d350c2426749bcd356116e21
sgautier-dev <sgautier.dev@gmail.com>
2025-01-29 18:54:40 +0400

    lang to fr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 	children: React.ReactNode
 }>) {
 	return (
-		<html lang="en">
+		<html lang="fr">
 			<body
 				className={`${montserrat.variable} antialiased mx-auto w-full max-w-screen-2xl font-montserrat`}
 			>
