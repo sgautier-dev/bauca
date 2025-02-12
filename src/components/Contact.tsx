88944c0aa04330e8e83672ef2733e63c7280a7ea
sgautier-dev <sgautier.dev@gmail.com>
2025-02-12 15:37:25 +0400

    contact form color modified

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -153,7 +153,7 @@ export default function Contact() {
 										autoComplete="given-name"
 										value={formData.firstName}
 										onChange={handleChange}
-										className="block w-full rounded-md bg-white px-3.5 py-2 text-base outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+										className="block w-full rounded-md bg-white px-3.5 py-2 text-base outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-lightGreen"
 										required
 									/>
 								</div>
@@ -173,7 +173,7 @@ export default function Contact() {
 										autoComplete="family-name"
 										value={formData.lastName}
 										onChange={handleChange}
-										className="block w-full rounded-md bg-white px-3.5 py-2 text-base outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+										className="block w-full rounded-md bg-white px-3.5 py-2 text-base outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-lightGreen"
 										required
 									/>
 								</div>
@@ -193,17 +193,14 @@ export default function Contact() {
 										autoComplete="email"
 										value={formData.email}
 										onChange={handleChange}
-										className="block w-full rounded-md bg-white px-3.5 py-2 text-base outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+										className="block w-full rounded-md bg-white px-3.5 py-2 text-base outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-lightGreen"
 										required
 									/>
 								</div>
 							</div>
 							<div className="sm:col-span-2">
 								<div className="flex justify-between text-sm/6">
-									<label
-										htmlFor="phone"
-										className="block font-semibold "
-									>
+									<label htmlFor="phone" className="block font-semibold ">
 										Tél
 									</label>
 									<p id="phone-description" className="text-gray-400">
@@ -218,7 +215,7 @@ export default function Contact() {
 										autoComplete="tel"
 										value={formData.phone}
 										onChange={handleChange}
-										className="block w-full rounded-md bg-white px-3.5 py-2 text-base outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+										className="block w-full rounded-md bg-white px-3.5 py-2 text-base outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-lightGreen"
 									/>
 								</div>
 							</div>
@@ -236,7 +233,7 @@ export default function Contact() {
 										rows={4}
 										value={formData.message}
 										onChange={handleChange}
-										className="block w-full rounded-md bg-white px-3.5 py-2 text-base outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+										className="block w-full rounded-md bg-white px-3.5 py-2 text-base outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-lightGreen"
 										required
 									/>
 								</div>
@@ -245,7 +242,7 @@ export default function Contact() {
 						<div className="mt-8 flex justify-end">
 							<button
 								type="submit"
-								className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+								className="rounded-md bg-lightGreen px-3.5 py-2.5 text-center text-sm font-semibold text-black shadow-sm hover:bg-lightGreen/60 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lightGreen"
 							>
 								{isExecuting ? (
 									<Loader2 className="h-5 w-5 animate-spin" />
